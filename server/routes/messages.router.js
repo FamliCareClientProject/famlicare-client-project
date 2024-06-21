2d0eeaa9bd157da6c4072b055079ec8141c67ff2
Janet Scanlon <janet.Lscanlon@gmail.com>
2024-06-20 23:05:39 -0500

    trying to integrate passport

--- server/routes/messages.router.js
+++ server/routes/messages.router.js
@@ -4,14 +4,17 @@ const router = express.Router();
 const { Server } = require("socket.io");
 const cors = require("cors")
 const pgAdapter = require("@socket.io/postgres-adapter")
+// const passport = require("passport")
+// const sessionMiddleware = require("../modules/session-middleware")
+
 //!  CHANGE BOILER PLATE TO REFLECT EXPRESS FRAME WORK NOT HTTP 
 //DEFINE A SEPERATE PORT FOR SOCKET.IO TO BE ON
 const PORT = process.env.PORT || 3000
 const app = express()
 app.use(cors())
+// app.use(sessionMiddleware);
 
-
-
+//declare the server and listen on PORT 
 const server = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
@@ -24,13 +27,42 @@ const io = new Server(server, {
   },
 });
 
+// //Share User Context with Socket.io 
+// function onlyForHandshake(middleware) {
+//   return (req, res, next) => {
+//     const isHandshake = req._query.sid === undefined;
+//     if (isHandshake) {
+//       middleware(req, res, next);
+//     } else {
+//       next();
+//     }
+//   };
+// }
+
+// io.engine.use(onlyForHandshake(sessionMiddleware));
+// io.engine.use(onlyForHandshake(passport.session()));
+// io.engine.use(
+//   onlyForHandshake((req, res, next) => {
+//     if (req.user) {
+//       next();
+//     } else {
+//       res.writeHead(401);
+//       res.end();
+//     }
+//   }),
+// );
+
+
+
 io.on('connection', (socket) => {
   //server side when a user is connected
   console.log('connected!')
   socket.on('new message', (message) => {
     //! no longer have access to req.user while using socket 
-    // const lovedOneId = req.user.loved_one_id
-    // const userId = req.user.id
+    //const lovedOneId = socket.request.user.id
+    
+    // const userId = socket.request.user.id
+    // console.log('user id is:', userId)
     const sqlText = `INSERT INTO messages("message_text")
                         VALUES ($1);`
     const sqlValues = [message]
